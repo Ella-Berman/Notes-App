57ffab6c6b13780c4ab9a45c2cbfe4b4ccc8d470
Ella-Berman <ellaberman9@gmail.com>
2024-06-23 11:53:48 -0700

    running notes app with ui and server

---
+++ notes-app-server/src/index.ts
@@ -0,0 +1,92 @@
+/* holds server and API code */
+
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/notes", async (req, res) => {
+    const notes = await prisma.note.findMany();
+    res.json(notes);
+});
+
+app.post("/api/notes", async(req, res) => {
+    const { title, content } = req.body;
+
+    if(!title || !content){
+        return res
+            .status(400)
+            .send("title and content fields required");
+    }
+
+    try{
+        const note = await prisma.note.create({
+            data: {title, content}
+        });
+        res.json(note);
+    }catch (error){
+        res
+            .status(500)
+            .send("Oops! Something went wrong.");
+    }
+
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+    const {title, content} = req.body;
+    const id = parseInt(req.params.id);
+
+    if(!title || !content){
+        return res
+            .status(400)
+            .send("Title and content are required fields.");
+    }
+    
+    if(!id || isNaN(id)) {
+        return res
+            .status(400)
+            .send("ID must be a valid integer.");
+    }
+
+    try{
+        const updatedNote = await prisma.note.update({
+            where: { id },
+            data: {title, content}
+        });
+        res.json(updatedNote);
+    } catch (error) {
+        res
+            .status(500)
+            .send("Oops! Something went wrong");
+    }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if(!id || isNaN(id)){
+        return res 
+            .status(400)
+            .send("ID must be a valid integer.");
+    }
+
+    try {
+        await prisma.note.delete({
+            where: { id },
+        });
+        res.status(204).send();
+    } catch (error) {
+        res
+            .status(500)
+            .send("Oops! Something went wrong.");
+    }
+});
+
+app.listen(5000, ()=> {
+    console.log("server running on localhost:5000");
+});
